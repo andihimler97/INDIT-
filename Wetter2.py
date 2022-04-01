cfc03eedcccd156e0ac5d4b9f462ca686bc73487
andihimler97 <102793501+andihimler97@users.noreply.github.com>
2022-04-01 11:22:36 +0200

    Add files via upload

---
+++ Wetter2.py
@@ -0,0 +1,17 @@
+wetter = input ("Bitte 'Regen' oder 'Sonne' eingeben:")
+
+
+
+if wetter.upper() == "REGEN":
+    print("heute leider nur Kellerparty möglich")
+    
+elif wetter.upper() == "SONNE":
+    print("Gartenparty", "juhuuuu")
+else:
+    print("Bitte 'Regen' oder 'Sonne' eingeben:")
+    
+
+    
+
+    
+    
