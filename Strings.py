74ffb11b3fc31b7b137a0b34a9a309d75f2f45ce
andihimler97 <102793501+andihimler97@users.noreply.github.com>
2022-05-28 14:16:00 +0200

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,24 @@
+zeichenkette =" Ich bin ein string"
+
+
+
+print(zeichenkette)
+print(zeichenkette[1])
+print(zeichenkette[10])
+
+
+wetter = input ("Bitte 'Regen' oder 'Sonne' eingeben:")
+
+if wetter.upper() == "Regen":
+    print("Keller")
+else:
+    print ("Terasse")
+    
+string_1 = "AnjFEER"
+kleinbuchstaben = string_1.lower()
+print(kleinbuchstaben)
+
+
+
+
+
